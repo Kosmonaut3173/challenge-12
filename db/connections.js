3a09da1764290365e0241d4329b409464c208c94
Kosmonaut 3173 <re.huerta3173@gmail.com>
2022-10-28 20:36:34 -0500

    adding starter for inquire portion of the challenge

---
+++ db/connections.js
@@ -0,0 +1,11 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = async() => {
+    let answers = await inquirer.prompt([
+        {
+            type:
+            
+        }
+    ])
+}
